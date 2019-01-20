64b0570948f848681ff3c211a8ed6e8ce5d0230c
Daniel Spiess <dspiess82@gmail.com>
2019-01-20 14:23:31 -0200

    Initialized some variables and creat check funtion to

--- app/src/main/java/com/example/android/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/android/moviesapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.android.moviesapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.moviesapp.utilities.NetworkUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NetworkUtils.check();
     }
 }
--- app/src/main/java/com/example/android/moviesapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/moviesapp/utilities/NetworkUtils.java
@@ -1,4 +1,17 @@
 package com.example.android.moviesapp.utilities;
 
+import android.util.Log;
+
 public class NetworkUtils {
+
+    //Tag for Logging
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    //Base URL for API request
+    private static final String MOVIE_DATABASE_URL_POPULAR =
+            "http://api.themoviedb.org/3/movie/popular";
+
+    public static void check(){
+        Log.v(TAG, MOVIE_DATABASE_URL_POPULAR);
+    }
 }
