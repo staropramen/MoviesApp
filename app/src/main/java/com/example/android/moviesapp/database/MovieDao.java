a433fb1a084f6c5e8d1bd752930681a02e6a6988
Daniel Spiess <dspiess82@gmail.com>
2019-01-31 13:43:14 -0200

    VR Prepares moviedao

--- app/src/main/java/com/example/android/moviesapp/database/MovieDao.java
+++ app/src/main/java/com/example/android/moviesapp/database/MovieDao.java
@@ -1,4 +1,26 @@
 package com.example.android.moviesapp.database;
 
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.ArrayList;
+
+@Dao
 public interface MovieDao {
+
+    @Query("SELECT * FROM movie ORDER BY movie_id")
+    ArrayList<MovieEntry> loadAllMovies();
+
+    @Insert
+    void insertMovie(MovieEntry movieEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateMovie(MovieEntry movieEntry);
+
+    @Delete
+    void deleteMovie(MovieEntry movieEntry);
 }
